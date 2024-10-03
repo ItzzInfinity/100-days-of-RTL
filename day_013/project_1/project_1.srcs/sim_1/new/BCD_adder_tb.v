1b3eac11c57bbb191952a69bcd6b38fe50d82870
ItzzInfinity <97271459+ItzzInfinity@users.noreply.github.com>
2024-10-03 21:59:18 +0530

    adding 13 th day of RTL

---
+++ day_013/project_1/project_1.srcs/sim_1/new/BCD_adder_tb.v
@@ -0,0 +1,39 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Anjan Prasad
+// Create Date: 10/04/2024 09:08:42 AM
+// Module Name: BCD_adder_tb
+//////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 1ps
+
+
+module BCD_adder_tb;
+
+    reg [3:0] X, Y; // 4-bit inputs
+    reg Cin;        // Carry input
+    wire [3:0] P; // 4-bit BCD sum
+    wire Cout;      // Carry output
+
+    // Instantiate the BCD adder
+    BCD_adder UUT (
+        .X(X), 
+        .Y(Y), 
+        .Cin(Cin), 
+        .P(P), 
+        .Cout(Cout)
+    );
+
+    initial begin
+        // Test cases
+        $monitor("X = %d, Y = %d, Cin = %b -> P = %d, Cout = %b (%d)", X, Y, Cin, P, Cout,Cout*10);
+        
+        // Initial input values
+        X = 4'b0000; Y = 4'b0000; Cin = 0;
+        #10 X = 4'b0011; Y = 4'b0101; Cin = 0;  // 3 + 5
+        #10 X = 4'b1001; Y = 4'b0101; Cin = 0;  // 9 + 5
+        #10 X = 4'b0110; Y = 4'b0111; Cin = 0;  // 6 + 7
+        #10 X = 4'b1001; Y = 4'b1001; Cin = 0;  // 9 + 9 
+        
+        #10 $finish;
+    end
+endmodule
---
+++ day_013/project_1/project_1.srcs/sources_1/new/IC7483.v
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Anjan Prasad
+// Create Date: 10/04/2024 09:08:42 AM
+// Module Name: IC7483
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module IC7483(
+input A0,
+input A1,
+input A2,
+input A3,
+input B0,
+input B1,
+input B2,
+input B3,
+input Cin,
+output S0,
+output S1,
+output S2,
+output S3,
+output Cout);
+wire C1,C2,C3;
+full_adder FA1(.a(A0), .b(B0), .cin(Cin), .sum(S0), .cout(C1));
+full_adder FA2(.a(A1), .b(B1), .cin(C1), .sum(S1), .cout(C2));
+full_adder FA3(.a(A2), .b(B2), .cin(C2), .sum(S2), .cout(C3));
+full_adder FA4(.a(A3), .b(B3), .cin(C3), .sum(S3), .cout(Cout));
+endmodule
